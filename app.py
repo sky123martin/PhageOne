0ff249099e4e8173c20909cff1806b51b1825fe7
Sky Martin <skymartin@Skys-MacBook-Pro.local>
2021-03-15 09:32:53 -0600

    remove import

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from application import app
-from application.setup import download_all_fastas, download_all_phage_genes
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
